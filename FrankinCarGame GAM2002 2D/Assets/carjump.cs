10c89fe0ba187d77fad83810aae7758c60d41937
Christian <christiancadogan@googlemail.com>
2019-11-25 17:35:08 +0000

    Iteration 3
    FIxed the jump physics by adding a position freeze script to temp freeze the position of the vehicle when jumping but not when falling

--- FrankinCarGame GAM2002 2D/Assets/carjump.cs
+++ FrankinCarGame GAM2002 2D/Assets/carjump.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class carjump : MonoBehaviour
 {
     public float playerSpeed;
-    public Vector3 gravity;
+   
     public float maxSpeed = 5f;
     public Vector3 velocity = Vector3.zero;
     public float fowardSpeed = 1f;
     public Rigidbody2D rb;
+    public Rigidbody2D rb1;
+    public List<wheelGrounded> listofwheels;
     public float thrust = 1f;
    private bool didPress = false;
+    private bool onTheGround;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetMouseButton(0))
+        if (Input.GetKeyDown(KeyCode.W))
         {
             didPress = true;
         }
@@ -23,20 +26,41 @@ public class carjump : MonoBehaviour
         {
             didPress = false;
         }
+        onTheGround = false;
+        foreach(wheelGrounded currentWheel in listofwheels)
+        {
+            if (currentWheel.isGrounded)
+            {
+                onTheGround = true;
+            }
+        }
     }
 
     void FixedUpdate()
     {
+
+        if (onTheGround)
+        {
+            gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+        }
+
         velocity.x = fowardSpeed;
         transform.position += velocity * Time.deltaTime;
         if (didPress == true)
         {
             {
+
+                gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+                rb1.AddForce(transform.up * thrust, ForceMode2D.Impulse);
                 rb.AddForce(transform.up * thrust, ForceMode2D.Impulse);
+               
             }
+            
             float amntToMove1 = playerSpeed * Time.deltaTime;
             transform.Translate(Vector3.up * amntToMove1);
         }
+
+        //gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
         velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
         transform.position += velocity * Time.deltaTime;
     }
---
+++ FrankinCarGame GAM2002 2D/Assets/wheelGrounded.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wheelGrounded : MonoBehaviour
+{
+    public bool isGrounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ground")
+        {
+            isGrounded = false;
+        }
+    }
+}
